e1a74536bf5e932bb02d2960c675714dad3f0ebb
Sobcat <771954909@qq.com>
2024-03-01 17:32:11 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div></div>
+  <div class="home">
+    <main>
+      <!-- 左侧组件列表 -->
+      <section class="left"></section>
+      <!-- 中间画布 -->
+      <section class="center"></section>
+      <!-- 右侧属性列表 -->
+      <section class="right"></section>
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
