1aaca83e5832d71d3cc21939ba121f85be58450a
Sobcat <771954909@qq.com>
2024-03-12 08:27:17 +0800

    update

--- src/components/custom-components/index.ts
+++ src/components/custom-components/index.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import AppTow from '@/AppTow.vue'
+// 这是不行的，对于vue3来说
 const app = createApp(AppTow)
 const components = ['VButton']
 components.forEach((e) => {
   app.component(e, () => import(`@/custom-components/${e}/Component`))
   app.component(e + 'Attr', () => import(`@/custom-components/${e}/Attr`))
 })
+
 console.log(app)
